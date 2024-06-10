ab96ec57b0336aba6b2d3cfc8fcdf49351b31fe2
Dr. Poornima G. Naik <31154391+pgnaik@users.noreply.github.com>
2024-06-10 05:56:15 -0800

    Update mydb.sql

--- dump/mydb.sql
+++ dump/mydb.sql
@@ -4,7 +4,7 @@ USE institute;
 CREATE TABLE `student` (
   `rollno` int(11) NOT NULL,
   `name` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+);
 
 
 INSERT INTO `Person` (`id`, `name`) VALUES
